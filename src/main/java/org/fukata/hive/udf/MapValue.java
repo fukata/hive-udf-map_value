3e268e122b3a01f6db2227e22979eefbb493be6c
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2013-09-17 13:16:14 +0900

    fixed map structure

--- src/main/java/org/fukata/hive/udf/MapValue.java
+++ src/main/java/org/fukata/hive/udf/MapValue.java
@@ -4,11 +4,10 @@ import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import java.util.Map;
 
-@Description(name = "map_value", value = "_FUNC_ extract value from map", extended = "")
+@Description(name = "map_value", value = "map_value extract value from map", extended = "")
 public class MapValue extends UDF {
     @SuppressWarnings("rawtypes")
-    //public String evaluate (Map map, Object...keys) {
-    public String evaluate (Map<String, Map<String, String>> map, String...keys) {
+    public String evaluate (Object map, Object...keys) {
         Object value = null;
         for ( Object key : keys ) {
             @SuppressWarnings("unchecked")
