cc98fc0cee428e8b5a7f9b0ae7d45ea8a614fc99
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2013-09-19 16:05:12 +0900

    refactor

--- src/main/java/org/fukata/hive/udf/MapValue.java
+++ src/main/java/org/fukata/hive/udf/MapValue.java
@@ -7,7 +7,7 @@ import java.util.Map;
 @Description(name = "map_value", value = "_FUNC_(map, keys) - extract value from map")
 public class MapValue extends UDF {
     @SuppressWarnings("rawtypes")
-    public String evaluate (Object map, Object...keys) {
+    Object doEvaluate (Object map, Object...keys) {
         Object value = null;
         for ( Object key : keys ) {
             @SuppressWarnings("unchecked")
@@ -16,18 +16,7 @@ public class MapValue extends UDF {
 
             value = m.get(key);
             if ( null == value ) return null;
-            
-            String clazzName = value.getClass().getName();
-            if ( "java.lang.String".equals(clazzName) ) {
-                return value.toString();
-            }
         }
-
-        String clazzName = value.getClass().getName();
-        if ( "java.lang.String".equals(clazzName) ) {
-            return value.toString();
-        }
-
         return value.toString();
     }
 }
--- src/main/java/org/fukata/hive/udf/MapValueString.java
+++ src/main/java/org/fukata/hive/udf/MapValueString.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 @Description(name = "map_value_string", value = "_FUNC_(map, keys) - extract value from map")
 public class MapValueString extends MapValue {
     public String evaluate (Object map, Object...keys) {
-        return super.evaluate(map, keys);
+        Object value = doEvaluate(map, keys);
+        return value == null ? null : value.toString();
     }
 }
